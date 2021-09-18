0e5db52586f63f2534492a60e51ee4b18ed1042b
raiiiz <rickdavidsangalang@gmail.com>
2021-09-18 22:45:10 +0800

    add books

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,12 +3,33 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 
+const favoritesBox = 'favorite_books';
+const List<String> books = [
+  'Harry Potter',
+  'To Kill a Mockingbird',
+  'The Hunger Games',
+  'The Giver',
+  'Brave New World',
+  'Unwind',
+  'World War Z',
+  'The Lord of the Rings',
+  'The Hobbit',
+  'Moby Dick',
+  'War and Peace',
+  'Crime and Punishment',
+  'The Adventures of Huckleberry Finn',
+  'Catch-22',
+  'The Sound and the Fury',
+  'The Grapes of Wrath',
+  'Heart of Darkness',
+];
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDir.path);
   Hive.registerAdapter(ContactAdapter());
-  final box = await Hive.openBox('myBox');
+  await Hive.openBox('myBox');
   runApp(MyApp());
 }
 
